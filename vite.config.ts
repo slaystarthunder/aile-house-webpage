bbed6b54e47061217ab15a2f07af21d6dcf34a93
slaystarthunder <72455891+slaystarthunder@users.noreply.github.com>
2025-12-01 01:41:22 +0100

    Update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
   plugins: [react()],
-  base: "/YOUR-REPO-NAME/", // 👈 replace with the *exact* repo name
+  base: "/aile-house-webpage/",
 });
